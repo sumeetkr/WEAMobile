c03f613eb1c61df4e8238fad7feb48b39da85ca1
Sumeet Kumar <kr.sumeet@gmail.com>
2014-09-25 16:00:46 -0700

    copied BootupBroadcastReceiver from our prototype application

--- app/src/main/java/sv/cmu/edu/weamobile/service/BootupBroadcastReceiver.java
+++ app/src/main/java/sv/cmu/edu/weamobile/service/BootupBroadcastReceiver.java
@@ -3,15 +3,22 @@ package sv.cmu.edu.weamobile.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-
+/**
+ * @author Maxim Kovalev
+ * As in http://stackoverflow.com/questions/6391902/how-to-start-an-application-on-startup
+ */
 public class BootupBroadcastReceiver extends BroadcastReceiver {
-    public BootupBroadcastReceiver() {
-    }
-
+    /* (non-Javadoc)
+      * @see android.content.BroadcastReceiver#onReceive(android.content.Context, android.content.Intent)
+      */
     @Override
     public void onReceive(Context context, Intent intent) {
-        // TODO: This method is called when the BroadcastReceiver is receiving
-        // an Intent broadcast.
-        throw new UnsupportedOperationException("Not yet implemented");
+        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+            Intent i = new Intent();
+            i.setClass(context, WEABackgroundService.class);
+            i.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP | Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startActivity(i);
+        }
+
     }
 }
