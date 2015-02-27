96f9749843c18c7d7f80191dacd206c8c3250d4f
Sumeet Kumar <kr.sumeet@gmail.com>
2015-02-27 18:26:47 -0500

    dummy change

--- app/src/main/java/sv/cmu/edu/weamobile/service/ActivityRecognitionService.java
+++ app/src/main/java/sv/cmu/edu/weamobile/service/ActivityRecognitionService.java
@@ -3,15 +3,13 @@ package sv.cmu.edu.weamobile.service;
 /**
  * Created by sumeet on 2/27/15.
  */
+import android.app.IntentService;
+import android.content.Intent;
+
 import com.google.android.gms.location.ActivityRecognitionResult;
 import com.google.android.gms.location.DetectedActivity;
 
-import android.app.IntentService;
-import android.content.Intent;
-import android.util.Log;
-
 import sv.cmu.edu.weamobile.utility.Logger;
-import sv.cmu.edu.weamobile.utility.WEAUtil;
 
 public class ActivityRecognitionService extends IntentService{
 
@@ -34,11 +32,6 @@ public class ActivityRecognitionService extends IntentService{
         }
     }
 
-    /**
-     * When supplied with the integer representation of the activity returns the activity as friendly string
-     * @param type the DetectedActivity.getType()
-     * @return a friendly string of the
-     */
     private static String getFriendlyName(int detected_activity_type){
         switch (detected_activity_type ) {
             case DetectedActivity.IN_VEHICLE:
